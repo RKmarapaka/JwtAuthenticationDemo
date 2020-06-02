c53c6a8825ba0697620bbedaa63991386b09052b
Elite <Elite@DESKTOP-GV0NG19>
2020-06-02 12:46:48 +0530

    Add project files.

---
+++ JwtAuthenticationDemo/Controllers/LoginController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JwtAuthenticationDemo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+//using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtAuthenticationDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration config;
+
+        public LoginController(IConfiguration config)
+        {
+            this.config = config;
+        }
+        public IActionResult Login(string username, string pass)
+        {
+            UserModel login = new UserModel();
+            login.UserName = username;
+            login.Password = pass;
+            IActionResult response = Unauthorized();
+
+            var user = AutheticateUser(login);
+
+            if (user != null)
+            {
+                var tokenStr = GenerateJsonWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+            return response;
+
+        }
+
+        private UserModel AutheticateUser(UserModel login)
+        {
+            UserModel user = null;
+            if (login.UserName == "marapaka" && login.Password == "123")
+            {
+                user = new UserModel { UserName = "marapaka", EmailAddress = "marapaka@gmail.com", Password = "123" };
+            }
+            return user;
+        }
+
+        private string GenerateJsonWebToken(UserModel userinfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,userinfo.UserName),
+                new Claim(JwtRegisteredClaimNames.Email,userinfo.EmailAddress),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: config["Jwt:Issuer"],
+                audience: config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials
+                );
+            var encodetoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodetoken;
+        }
+
+        [Authorize]
+        [HttpPost("Post")]
+        public string Post()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+            var userName = claim[0].Value;
+            return "Welcome to: " + userName;
+        }
+
+        [Authorize]
+        [HttpGet("GetValue")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Value1", "Value2", "Value3" };
+        }
+    }
+}
